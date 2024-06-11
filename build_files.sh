4a2776dc6bc6c09781f493698acbbc9d8d034ddc
Travcort <tarvkoome@gmail.com>
2024-06-11 19:59:01 +0300

    Modified the Python Version

--- build_files.sh
+++ build_files.sh
@@ -5,7 +5,7 @@ python 3.9 -m venv virtual
 .\virtual\Scripts\activate
 
 # Installing the requirements
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Collecting the static files
 python3.9 manage.py collectstatic
