11c9739dc42c4315e1a7f4e954b50b46b9ec0cf8
Travcort <tarvkoome@gmail.com>
2024-06-11 18:37:26 +0300

    Modified the build script

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,8 @@
+# Activating the virtual environment
+virtual\Scripts\activate
+
+# Installing the requirements
 pip install -r requirements.txt
+
+# Collecting the static files
 python3.12 manage.py collectstatic
