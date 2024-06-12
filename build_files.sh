4c37cc8a15c1e8024f96ce8e69b8287f0dd5caea
Travcort <tarvkoome@gmail.com>
2024-06-12 09:35:10 +0300

    Modified the Script

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Creating a virtual environment
-python -m venv virtual
+python3 -m venv virtual
 
 # Activating the virtual environment
 .\virtual\Scripts\activate
 
 # Installing the requirements
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Collecting the static files
-python3.9 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
