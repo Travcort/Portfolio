7b6c4ff12e3520bae0a8a1591d4a410b9f9808ea
Travcort <tarvkoome@gmail.com>
2024-06-12 10:08:24 +0300

    Minor changes after Deployment

--- build_files.sh
+++ build_files.sh
@@ -8,5 +8,8 @@ python3 -m venv virtual
 # Installing the requirements
 python3 -m pip install -r requirements.txt
 
+# Run the Migrations
+python3 -m manage.py migrate --noinput
+
 # Collecting the static files
 python3 manage.py collectstatic --noinput
