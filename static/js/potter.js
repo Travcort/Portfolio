0b54474ae62566c82ebcb3f9afd3b06f67692786
Travcort <tarvkoome@gmail.com>
2024-06-18 00:16:12 +0300

    Improved Styling and Scripting

--- static/js/potter.js
+++ static/js/potter.js
@@ -1,15 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    // The Refresh Button
-    var refreshButton = document.getElementById("refresh");
-    if(refreshButton) {
-        refreshButton.addEventListener("click", function() {
-            console.log("Refreshed!");
-            location.reload();
-        });
+    // The TypeWriter Effect on the Landing page heading
+    if (document.getElementById("landing-header")) {
+        function typeWriter() {
+            let header = document.getElementById("landing-header");
+            let charIndex = 0;
+            const headerText = header.textContent;
+
+            headerText.textContent = "";
+
+            function type() {
+                if (charIndex < headerText.length) {
+                    headerText.textContent += headerText.charAt(charIndex);
+                    charIndex ++;
+                    setTimeout(type, 75);
+                }
+            }
+            // Calling the Function
+            type();
+        }
+        typeWriter();
     }
-    else {
-        console.error("Refresh Button not found!");
+
+    // The Refresh Button
+    if (document.getElementById("refresh")) {
+        var refreshButton = document.getElementById("refresh");
+        if(refreshButton) {
+            refreshButton.addEventListener("click", function() {
+                console.log("Refreshed!");
+                location.reload();
+            });
+        }
+        else {
+            console.error("Refresh Button not found!");
+        }
     }
 
     // Resizing the textarea element in books.html and spells.html
