d544fe9267b1cfe7684ce45fd4ae3537208d2b80
Travcort <tarvkoome@gmail.com>
2024-06-19 13:26:18 +0300

    Added Responsiveness and Better Styling

---
+++ staticfiles_build/static/js/apis-script.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // The TypeWriter Effect on the Landing page heading
+    if (document.getElementById("landing-header")) {
+        function typeWriter() {
+            let header = document.getElementById("landing-header");
+            let charIndex = 0;
+            const headerText = header.textContent;
+
+            headerText.textContent = "";
+
+            function type() {
+                if (charIndex < headerText.length) {
+                    headerText.textContent += headerText.charAt(charIndex);
+                    charIndex ++;
+                    setTimeout(type, 75);
+                }
+            }
+            // Calling the Function
+            type();
+        }
+        typeWriter();
+    }
+
+    // The Refresh Button
+    if (document.getElementById("refresh")) {
+        var refreshButton = document.getElementById("refresh");
+        if(refreshButton) {
+            refreshButton.addEventListener("click", function() {
+                console.log("Refreshed!");
+                location.reload();
+            });
+        }
+        else {
+            console.error("Refresh Button not found!");
+        }
+    }
+
+    // Resizing the textarea element in books.html and spells.html
+    if (document.getElementById("text-area")) {
+        var textarea = document.getElementById('text-area');
+        textarea.style.height = "auto";
+        textarea.style.height = (textarea.scrollHeight) + "px";
+    }
+
+    
+});
+
